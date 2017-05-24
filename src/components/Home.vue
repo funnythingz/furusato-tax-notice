2c4310647152300a2f7db5029aafa3aedfab78ad
funnythingz <funnythingz69+github@gmail.com>
2017-05-25 01:06:56 +0900

    binding data

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,60 +6,62 @@
         <i class="fa fa-gift" aria-hidden="true"></i>
         ふるさと納税新着情報
       </h1>
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          <h1 class="h3">
-            佐賀県大町町
-            <small>
-              2017年5月24日（水）21時33分
-            </small>
-          </h1>
-        </div>
-        <div class="panel-body">
-          <div class="row">
-            <div class="col-md-3 text-center">
-              <img src="https://img.furusato-tax.jp/img/x/notifications/20170524/n_459299c62530313d0ccaf824ae5a846538c4a25e.jpg" alt="" class="spacing">
-            </div>
-            <div class="col-md-9">
-              <p class="lead">
-                希少部位フィレ入手！極上プレミアムヒレステーキ！
-              </p>
-              <p>
-                ロースは美味しいけど、脂身が気になってなかなか沢山の量は食べれないという方へ。
-                <br>
-                極上のヒレをご用意いたしました。
-                <br>
-                脂身が少なく、あっさり食べることができます。
-                <br>
-                これは絶品です。
-              </p>
-              <p>
-                A5、A4の最上等級の九州産黒毛和牛。
-                </br>
-                ヒレ肉は、牛1頭からわずか5％程度しかとれない希少なお肉です。
-              </p>
-              <p>
-                他ではなかなか真似できない贅沢な逸品。
-              </p>
-              <p>
-                佐賀の肥沃な土地で育った和牛をご堪能下さい。
-              </p>
-              <p>
-                ご自宅用はもちろん、贈答用にも喜ばれること間違いありません。
-                <br>
-                塩だけ振って焼いて、肉のうまさを堪能してください。
-              </p>
-              <p class="text-right">
-                <a href="#">
-                  <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
-                  佐賀県大町町のふるさと納税はこちら
-                </a>
-              </p>
+      <template
+        v-for="item in search_list"
+        v-bind:item="item"
+        >
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h1 class="h3">
+              <a :href="item.link">
+                {{item.author}}
+                <small>
+                  {{item.pubDate}}
+                </small>
+              </a>
+            </h1>
+          </div>
+          <div class="panel-body">
+            <div class="row">
+              <div class="col-md-12">
+                <p class="lead">
+                  <a :href="item.link">
+                    {{item.title}}
+                  </a>
+                </p>
+                <p v-html="item.content" class="content"></p>
+                <p class="text-right">
+                  <a :href="item.link">
+                    <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
+                    {{item.author}}のふるさと納税はこちら
+                  </a>
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </template>
     </div>
   </section>
 
 </template>
+
+<script>
+import Axios from 'axios'
+
+const jsonUri = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.furusato-tax.jp%2Ffeed%2Fnotifications.xml';
+
+export default {
+  data() {
+    return {
+      search_list: []
+    }
+  },
+  created() {
+    Axios.get(jsonUri).then((res) => {
+      this.search_list = res.data.items;
+    });
+  }
+}
+
+</script>
